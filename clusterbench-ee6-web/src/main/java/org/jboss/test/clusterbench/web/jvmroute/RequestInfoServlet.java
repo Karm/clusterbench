ed9ff256720dc2617b389a22966cdc36f3c28e20
Michal Karm Babacek <mbabacek@redhat.com>
2013-02-01 18:31:25 +0100

    Added /requestinfo/*

--- clusterbench-ee6-web/src/main/java/org/jboss/test/clusterbench/web/jvmroute/RequestInfoServlet.java
+++ clusterbench-ee6-web/src/main/java/org/jboss/test/clusterbench/web/jvmroute/RequestInfoServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.annotation.WebServlet;
 
 import org.jboss.test.clusterbench.common.jvmroute.CommonRequestInfoServlet;
 
-@WebServlet(name = "RequestInfoServlet", urlPatterns = { "/requestinfo" })
+@WebServlet(name = "RequestInfoServlet", urlPatterns = { "/requestinfo", "/requestinfo/*" })
 public class RequestInfoServlet extends CommonRequestInfoServlet {
   private static final long serialVersionUID = -7788063893669970638L;
 }
