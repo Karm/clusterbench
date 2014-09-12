0c9546ac67949add9d26ab7b7cf55f6960063846
Michal Karm Babacek <mbabacek@redhat.com>
2014-09-12 11:11:31 +0200

    Fixing minor bugs...

--- clusterbench-ee5-web/src/main/java/org/jboss/test/clusterbench/web/jvmroute/HTTPCodeServlet.java
+++ clusterbench-ee5-web/src/main/java/org/jboss/test/clusterbench/web/jvmroute/HTTPCodeServlet.java
@@ -3,8 +3,6 @@ package org.jboss.test.clusterbench.web.jvmroute;
 import org.jboss.test.clusterbench.common.jvmroute.CommonHTTPCodeServlet;
 import org.jboss.test.clusterbench.common.jvmroute.CommonRequestInfoServlet;
 
-import javax.servlet.annotation.WebServlet;
-
 public class HTTPCodeServlet extends CommonHTTPCodeServlet {
     // Silence is golden
 }
