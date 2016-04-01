f83ce5f0512dea775c95e5cbd4df25154a0bd205
Michal Karm Babacek <karm@redhat.com>
2016-04-01 12:20:48 +0200

    invalidation

--- clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/session/CommonHttpSessionServlet.java
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/session/CommonHttpSessionServlet.java
@@ -1,44 +1,50 @@
 package org.jboss.test.clusterbench.common.session;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.jboss.test.clusterbench.common.SerialBean;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.jboss.test.clusterbench.common.SerialBean;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CommonHttpSessionServlet extends HttpServlet {
-  private static final long serialVersionUID = 6413490751329934994L;
-  private static final Logger log = Logger.getLogger(CommonHttpSessionServlet.class.getName());
-  public static final String KEY = CommonHttpSessionServlet.class.getName();
+    private static final long serialVersionUID = 6413490751329934994L;
+    private static final Logger log = Logger.getLogger(CommonHttpSessionServlet.class.getName());
+    public static final String KEY = CommonHttpSessionServlet.class.getName();
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    HttpSession session = req.getSession(true);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
 
-    if (session.isNew()) {
-      log.log(Level.INFO, "New session created: {0}", session.getId());
-      session.setAttribute(KEY, new SerialBean());
+        if (session.isNew()) {
+            log.log(Level.INFO, "New session created: {0}", session.getId());
+            session.setAttribute(KEY, new SerialBean());
+        }
+
+        SerialBean bean = (SerialBean) session.getAttribute(KEY);
+
+        resp.setContentType("text/plain");
+
+        int serial = bean.getSerial();
+        bean.setSerial(serial + 1);
+
+        // Now store bean in the session
+        session.setAttribute(KEY, bean);
+
+        resp.getWriter().print(serial);
+
+        if (req.getParameter("invalidate") != null) {
+            log.log(Level.INFO, "Invalidating: {0}", session.getId());
+            session.invalidate();
+        }
     }
 
-    SerialBean bean = (SerialBean) session.getAttribute(KEY);
-
-    resp.setContentType("text/plain");
-
-    int serial = bean.getSerial();
-    bean.setSerial(serial + 1);
-
-    // Now store bean in the session
-    session.setAttribute(KEY, bean);
-
-    resp.getWriter().print(serial);
-  }
-
-  @Override
-  public String getServletInfo() {
-    return "Servlet using Session to store object with the serial.";
-  }
+    @Override
+    public String getServletInfo() {
+        return "Servlet using Session to store object with the serial.";
+    }
 }
